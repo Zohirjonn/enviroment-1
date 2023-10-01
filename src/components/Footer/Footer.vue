1b8cf61203f492a37dc98e22153dccd2d819411b
Zohirjonn <120003182+Zohirjonn@users.noreply.github.com>
2023-10-01 15:45:34 +0500

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-right mr-6 mb-4 text-white-3 text-sm">Creater by Zohirjon</div>
+  <div class="text-right mr-6 mb-4 text-white-3 text-sm">Creater by Z&F</div>
 </template>
 <script>
 export default {};
